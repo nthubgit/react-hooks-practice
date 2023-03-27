a9861ef84db4146664f6dcd5e3d05fd3db85057f
nthubgit <neltyler@gmail.com>
2023-03-27 14:31:57 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,13 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const NoteApp = () => {
+//   const [notes, setNotes] = useState([]);
+//   const [title, setTitle] = useState("");
+//   const [body, setBody] = useState("");
+
+//   const addNote = (e) => {
+//     e.preventDefault();
+//     setNotes([
+//       ...notes,
+//       {
+//         title,
+//         body
+//       },
+//     ]);
+//     setTitle("");
+//     setBody("");
+//   };
+
+//   const removeNote = (title) => {
+//     setNotes(notes.filter((note) => note.title !== title))
+//   }
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       {notes.map((note) => (
+//         <div key={note.title}>
+//           <h3>{note.title}</h3>
+//           <h4>{note.body}</h4>
+//           <button onClick ={() => removeNote(note.title)}>x</button>
+//         </div>
+//       ))}
+//       <p>Add Note</p>
+//       <form onSubmit={addNote}>
+//         <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+//         <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+//         <button>add note</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("");
+
+  // const increment = () => {
+  //   setCount(count + 1)
+  // }
+
+  return (
+    <div>
+      <p>
+        The current {text || "count"} is {count}.
+      </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 0,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App count={0}/>
   </React.StrictMode>
 );
 
